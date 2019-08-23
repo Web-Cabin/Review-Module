406f815443e98d97cd2f89968c6113b8a7741adc
Daniel Kang <danieldonghoonkang@gmail.com>
2019-08-23 16:32:26 -0700

    Created an API call to render my data onto client

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const db = require('../db');
-const app = require('express');
+const express = require('express')
 
-app.get('/api/reviews', (req, res) => {
-  res.send()
+const app = express();
+const PORT = 3000;
+
+app.get('/api/reviews/:id', (req, res) => {
+  db.ReviewComment.find({listingID: req.params.id}, (error, data) => {
+    if(error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(data);
+    }
+  })
 })
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
