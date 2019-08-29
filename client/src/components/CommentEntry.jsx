93558635f65949fc468f6b20c4b31f3ffae9b593
Daniel Kang <danieldonghoonkang@gmail.com>
2019-08-29 12:26:47 -0700

    Basic dynamic list render from DB

---
+++ client/src/components/CommentEntry.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CommentEntry = (props) => (
+  <div>
+    <div>Hello</div>
+    <div>{props.data.name}</div>
+    <div>{props.data.date}</div>
+    <div>{props.data.message}</div>
+    <div>{props.data.profilePic}</div>
+  </div>
+);
+
+export default CommentEntry;
